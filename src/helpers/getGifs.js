2289c9c4258d10e9b9c805ed1a087f48a89e22d4
Jonatan Atencio <aa.jonatan@gmail.com>
2024-03-23 19:24:11 -0300

    se corrigio https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@
 
 export const getGifs = async (category) => {
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=o2WOSlVA39Jrx7kNdKu3KzsXwNZER3EF`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=o2WOSlVA39Jrx7kNdKu3KzsXwNZER3EF`
     const resp = await fetch(url);
     const { data } = await resp.json();
 
